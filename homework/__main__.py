311c8a27aa7bd89887ec24d0f68881671d3d9b5c
jamurciat <jamurciat@unal.edu.co>
2025-05-24 08:01:12 -0500

    homework1

---
+++ homework/__main__.py
@@ -0,0 +1,8 @@
+"""Entry point for the homework package."""
+
+# python3 -m homework --model elasticnet --alpha 0.9 --l1_ratio 0.1
+
+from .src.main import main
+
+if __name__ == "__main__":
+    main()
